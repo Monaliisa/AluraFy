b98058136bcbcb5bb3e76d0bb01e2ea047c5cd62
Lorena <77021882+loresgarcia@users.noreply.github.com>
2024-09-16 17:35:15 -0300

    feat: music player component added

---
+++ src/components/MusicPlayer/index.jsx
@@ -0,0 +1,34 @@
+import { Delete, FavoriteBorder } from '@mui/icons-material';
+
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import "./MusicPlayer.css";
+
+const MusicPlayer = () => {
+
+  return (
+    <div className="music-player">
+      <div className="track-info">
+        <img src="billie-album.jpg" alt="Capa do álbum" className="album-art" />
+        <div className="track-details">
+          <div className="song-info">
+            <h3>birds of a feather</h3>
+            <p>billie eilish</p>
+          </div>
+          <div className="icons">
+            <FavoriteBorder />
+            <Delete />
+          </div>
+        </div>
+      </div>
+
+      <AudioPlayer
+        autoPlay={false}
+        src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+        onPlay={() => console.log("onPlay")}
+      />
+    </div>
+  );
+};
+
+export default MusicPlayer;
