e3198c9974574e9a8155db1ce19a1c6fb54fa4f4
Rafaela Silverio <rafaelapetelin@gmail.com>
2024-09-19 16:08:01 -0300

    feat: search message component added

---
+++ src/components/SearchMessage/index.jsx
@@ -0,0 +1,11 @@
+import "./SearchMessage.css";
+
+const SearchMessage = () => {
+    return(
+        <section className="container">
+            <h3>Play what you love</h3>
+            <p>Search for songs and artists</p>
+        </section>
+    )
+}
+export default SearchMessage;
