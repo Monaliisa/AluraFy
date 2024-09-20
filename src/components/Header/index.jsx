98f9aa89613d57bf7a3ae2d208787514c5fb1752
Victor Costa <victorcosta123vcs@gmail.com>
2024-09-20 16:23:08 -0300

    feat - add dark mode functionality

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,32 @@
-import "./Header.css";
-import Search from "../Search";
+import { useState } from 'react';
+import './Header.css';
+import Search from '../Search';
+import darkIcon from '../../assets/dark-icon.svg'; 
+import lightIcon from '../../assets/light-icon.svg'; 
+import addIconDark from '../../assets/add-dark.svg'; 
+import addIconLight from '../../assets/add-light.svg'; 
+import logoLight from '../../assets/logo-light.svg';
+import logoDark from '../../assets/logo-dark.svg'; 
 
 const Header = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    
+    document.documentElement.classList.toggle('dark-mode', !isDarkMode);
+  };
+
   return (
-    <header className="header">
-      <img className="icon-header" src="/alurafyicon.svg" alt="Logo" />
+    <header className={`header ${isDarkMode ? 'dark' : 'light'}`}>
+       <img className="icon-header" src={isDarkMode ? logoDark : logoLight} alt="Logo" />
       <Search />
       <div className="butons">
         <a href="#">
-          <img src="/Add.svg" alt="Botão para adicionar músicas" />
+          <img src={isDarkMode ? addIconDark : addIconLight} alt="Botão para adicionar músicas" />
         </a>
-        <a href="#">
-          <img src="/DarkMode.svg" alt="Botão de modo Escuro" />
+        <a href="#" onClick={toggleDarkMode}>
+          <img src={isDarkMode ? lightIcon : darkIcon} alt="Botão de modo Escuro" />
         </a>
       </div>
     </header>
