26bab406546266e7f09f8234a97ffa13f46daed9
Lorena <77021882+loresgarcia@users.noreply.github.com>
2024-09-23 10:22:11 -0300

    feat: pages folder added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import './App.css'
-import MusicPlayer from './components/MusicPlayer'
-
-import CardList from './components/CardList'
-// import SearchMessage from './components/SearchMessage'
 import Header from './components/Header'
+
 
 function App() {
   return (
     <>
-      <Header/>
-      <CardList />
-      <MusicPlayer />
-      {/* <SearchMessage/> */}
-
-
-      
+      <Header/>   
     </>
   )
 }
---
+++ src/pages/Forms/index.jsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header"
+import MusicForms from "../../components/MusicForms"
+import "./Forms.css"
+
+const Forms = () => {
+    return (
+        <>
+            <Header />
+            <MusicForms />
+        </>
+    )
+}
+
+
+export default Forms;
