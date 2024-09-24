2642a51c12e471a234e1c4dad1c80783babe743e
armanoalves <armanobarrosalvesh@gmail.com>
2024-09-24 16:52:31 -0300

    build: ajuste no arquivo de configuração do banco de dados

--- database.js
+++ database.js
@@ -1,8 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite'
-});
+// Verifica se a variável de ambiente DATABASE_URL está definida
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Configuração do Sequelize com base no ambiente
+const sequelize = new Sequelize(
+  isProduction
+    ? process.env.DATABASE_URL // Usando a string de conexão do PostgreSQL para produção
+    : {
+        dialect: 'sqlite',
+        storage: './database.sqlite' // Usando SQLite para desenvolvimento/testes
+      }
+);
 
 module.exports = sequelize;
