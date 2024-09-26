dffd0cdd7e105f35ff6ea0d4e3de1a745e54afd1
renan lima <renangenito@hotmail.com>
2024-09-26 15:14:00 -0300

    feat: Adicionando o cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors'); // Importa o cors
 const routes = require('./api/routes');
-
 const { swaggerUi, swaggerDocs } = require('./api/utils/swaggerConfig');
 
 const app = express();
 const port = 3000;
+
+// Libera o CORS para todas as origens
+app.use(cors());
 
 routes(app);
 
@@ -12,6 +15,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(port, () => {
     console.log(`API rodando no http://localhost:${port} Acesse a API na rota http://localhost:3000/api-docs`);
-})
+});
 
 module.exports = app;
